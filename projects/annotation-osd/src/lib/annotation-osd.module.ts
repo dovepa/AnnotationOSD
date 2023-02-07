9df8a209636e60152d8c46ff1baf912557317b38
Dove Palombo <dove.palombo@gmail.com>
2023-02-07 13:08:55 +0100

    Auto-generated commit [ci skip]

--- projects/annotation-osd/src/lib/annotation-osd.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { AnnotationOSDService } from './annotation-osd.service';
-
-@NgModule({
-    imports: [],
-    providers : [AnnotationOSDService],
-})
-export class AnnotationOSDModule { }
--- projects/annotation-osd/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of annotation-osd
- */
-
-export * from './lib/annotation-osd.model';
-export * from './lib/annotation-osd.service';
-export * from './lib/annotation-osd.module';
